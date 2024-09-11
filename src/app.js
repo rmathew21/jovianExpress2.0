b2cb6b18a6940d08c9afc10a81cbd2d53a65ddcd
rob <robmathew21@gmail.com>
2024-09-11 09:21:15 -0500

    added src/pages/index.html with prev index file, added path utility in app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.sendFile(path.join(__dirname, 'pages/index.html'));
 });
 
 const port = process.env.PORT || 3000;
