346e03ce38c6f642c4fa5d8d0f60d856386506b9
rob <robmathew21@gmail.com>
2024-09-11 14:56:44 -0500

    added jobs list for mobile in index.mustache

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
 const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+    // res.sendFile(path.join(__dirname, 'pages/index.html'));
+    res.render('index', { jobs: JOBS });
+    // console.log(JOBS);
 });
 
 const port = process.env.PORT || 3000;
