347909abf205e206bcbe2c527b7590cbd3dc57e4
rob <robmathew21@gmail.com>
2024-09-11 23:56:16 -0500

    set up mailOptions and transporter.sendMail in app.js

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.get('/jobs/:id', (req, res) => {
 
     const matchedJob = JOBS.find(job => job.id.toString() === id);
     res.render('job', { job: matchedJob});
-    console.log('matchedJob', matchedJob);
+    // console.log('matchedJob', matchedJob);
 });
 
 const transporter = nodemailer.createTransport({
@@ -40,6 +40,39 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_ID,
         pass: process.env.EMAIL_PASSWORD
     }
+});
+
+app.post('/jobs/:id/apply', (req, res) => {
+    const { name, email, phone, dob, coverletter } = req.body;
+    const id = req.params.id;
+    const matchedJob = JOBS.find(job => job.id.toString() === id);
+    console.log('req.body', req.body);
+    console.log('matchedJob', matchedJob);
+
+
+const mailOptions = {
+    from: process.env.EMAIL_ID,
+    to: process.env.EMAIL_ID,
+    subject: `New Application for ${matchedJob.title}`,
+    html: `
+    <p><strong>Name:</strong> ${name}</p>,
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Phone:</strong> ${phone}</p>,
+    <p><strong>Date of Birth:</strong> ${dob}</p>,
+    <p><strong>Cover Letter:</strong> ${coverletter}</p>
+    `
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.error(error);
+        res.status(500).send('Error sending email');
+    } else {
+        console.log('Email sent: ' + info.response);
+        res.status(200).send('Email sent successfully');
+        res.render('Email sent successfully');
+    }
+});
 });
 
 const port = process.env.PORT || 3000;
